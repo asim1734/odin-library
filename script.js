6836b7bcd75142311e53e4cebdfe8779bd0d0c01
asim1734 <asim.rupani@gmail.com>
2024-11-21 20:41:24 +0530

    Add function to generate cards and display books

---
+++ script.js
@@ -0,0 +1,45 @@
+const myLibrary = [];
+
+function Book(title, authorName, publishingDate) {
+    this.title = title;
+    this.authorName = authorName;
+    this.publishingDate = publishingDate;
+}
+
+function addBookToLibrary(title, authorName, publishingDate) {
+  const book = new Book(title, authorName, publishingDate);
+  myLibrary.push(book);
+}
+
+function displayBooks(){
+    const booksContainer = document.querySelector(".books-container");
+    for( var i = 0 ; i < myLibrary.length ; i++){
+        booksContainer.appendChild(generateCard(myLibrary[i]));
+    }
+}
+
+function generateCard(book){
+    const bookCard = document.createElement('div');
+    bookCard.classList.add("book-card");
+
+    const titleP = document.createElement('p');
+    const nameP = document.createElement('p');
+    const dateP = document.createElement('p');
+
+    titleP.textContent += "Title: " + book.title;
+    nameP.textContent += "Author name: " + book.authorName;
+    dateP.textContent += "Publishing date: " + book.publishingDate;
+
+    bookCard.appendChild(titleP);
+    bookCard.appendChild(nameP);
+    bookCard.appendChild(dateP);
+
+    return bookCard;
+
+}
+
+
+addBookToLibrary("hello", "hi ", "23");
+addBookToLibrary("hi", "je;p", "er");
+displayBooks();
+console.log(myLibrary)
